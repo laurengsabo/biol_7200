a288b518a9e32b36fe907613567b59bf67e91b2d
Lauren Sabo <lsabo8@gatech.edu>
2026-09-21 20:46:21 -0400

    finished find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 # Usage: find_homologs.sh <query file> <subject file> <output file>
+
+tblastn -query $1 -subject $2 -outfmt "6 qseqid sseqid pident length qlen" | awk '$3 > 30 && $4 > 0.9*$5' > $3
+wc -l < $3
